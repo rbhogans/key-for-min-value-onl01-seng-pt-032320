400b91153902f8ccaf542fce8e40e122d26db78d
Rabiah Hogans <hogansrabiah@gmail.com>
2020-04-02 04:41:46 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,4 +4,12 @@
 def key_for_min_value(name_hash)
 smallest value=nil
 smallest key=nil
+name_hash.each do |key,value|
+    if smallest_value==nil || value<smallest_value
+      smallest_value=value
+      smallest_key=key
+    end
+  end
+    smallest_key
+end
 end
